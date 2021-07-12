9192c40c546fd594002ef00c951efeb2feb5ac2f
Jesswin Chetnani <64923931+jesswin@users.noreply.github.com>
2021-07-12 23:29:40 +0530

    customAuthContext

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,6 +1,43 @@
-import React  from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: () => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userLogged = localStorage.getItem("isLoggedIn");
+    // console.log("FIRST");
+    if (userLogged === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
 export default AuthContext;
